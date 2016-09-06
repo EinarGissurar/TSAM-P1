01c201af036092658b13fbfb307c5fbf41982d13
einargizz <einarog05@ru.is>
2016-09-06 18:43:13 +0000

    Main server

---
+++ tftpd.c
@@ -0,0 +1,130 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int sockfd, opcode, mode_pointer, block_code, buffer, i;
+	struct sockaddr_in server, client;
+	char message[516], reply[516], stream_path[100];
+	char* filename;
+	char* mode;
+	int port_number = strtol(argv[1], NULL, 10);
+	FILE *data;
+
+	/* Create and bind a TCP socket */
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	/* Network functions need arguments in network byte order instead of 
+	host byte order. The macros htonl, htons convert the values. */
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(port_number);
+	bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+
+	fprintf(stdout, "The server is on\n");
+	//fprintf(stdout, "argv[0] is: %s \n", argv[0]);
+	//fprintf(stdout, "argv[1] is: %s \n", argv[1]);
+	fprintf(stdout, "argv[2] is: %s \n", argv[2]);
+	//fprintf(stdout, "argv[3] is: %s \n", argv[3]);
+	//fprintf(stdout, "Port number is: %i \n", port_number);
+	
+    fflush(stdout);
+
+    listen(sockfd, 1);
+    
+
+	for(;;) {
+
+		sleep(1);
+		fprintf(stdout, "Waiting...\n");
+
+		/* We first have to accept a TCP connection, connfd is a fresh
+           handle dedicated to this connection. */
+		fprintf(stdout, "socklen_t len\n");
+        socklen_t len = (socklen_t) sizeof(client);
+      
+        /* Receive from connfd, not sockfd. */
+        fprintf(stdout, "ssize_t\n");
+        ssize_t n = recvfrom(sockfd, message, sizeof(message) - 1, 0, 
+        	(struct sockaddr *) &client, &len);
+
+        opcode = message[1];
+        fprintf(stdout, "Opcode is: %d\n", opcode);
+
+        //Clean reply message;
+        memset(reply,0,strlen(reply));
+    	switch(opcode) {
+    		case 1:
+    			//message to read/write
+    			filename = &message[2];
+
+    			//path to stream
+    			memset(stream_path, 0, strlen(stream_path));
+    			strcpy(stream_path, argv[2]);
+    			strcat(stream_path, "/");
+    			strcat(stream_path, filename);
+
+		        //find mode
+        		for (mode_pointer = 2; message[mode_pointer] != '\0'; mode_pointer++) {}
+        		mode_pointer++;
+    			mode = &message[mode_pointer];
+
+		        fprintf(stdout, "Filename is: %s\n", filename);
+    		    fprintf(stdout, "Mode is %s\n", mode);
+    		    fprintf(stdout, "Path is %s\n", stream_path);
+    		    fflush(stdout);
+
+    		    if ((data = fopen(stream_path, "r")) == NULL) {
+    		    	fprintf(stdout, "File not found\n");
+
+    		    	reply[1] = 5 & 0xff;
+    		    	reply[3] = 1 & 0xff;
+    		    	char error[] = "File not found\0";
+    		    	for (i = 0; i < sizeof(error); i++) {
+    		    		reply[4+i] = error[i];
+    		    	}
+    		    }
+    		    else {
+    		    	fprintf(stdout, "File found\n");
+
+    		    	buffer = 0;
+
+    		    	reply[1] = 3 & 0xff;
+    		    	reply[3] = 1 & 0xff;
+    		    	while ((c = fgetc(data)) != EOF && buffer < 512) {
+    		    		reply[buffer+4] = c &0xff;
+                        buffer++;
+                    	puts (reply);
+    		    	}
+    		    }
+    		    sendto(sockfd, reply, sizeof(reply)-1, 0, 
+    		    		(struct sockaddr *) &client, (socklen_t) sizeof(client));
+
+
+    		break;
+    		case 2:
+    		break;
+    		case 3:
+    		break;
+    		case 4:
+    		break;
+    		case 5:
+    		break;
+    	}
+		
+        /* Close the connection. */
+        //shutdown(connfd, SHUT_RDWR);
+        //close(connfd);
+
+
+	}
+
+	return 0;
+}
