fbe961827054079f9f2c8d8b63ca325ceb1987da
Peter Hostačný <hostacny.peter@gmail.com>
2016-09-13 22:03:02 +0200

    fix SIGSEGV, refactor code

--- src/tftpd.c
+++ src/tftpd.c
@@ -19,35 +19,37 @@
 #define ACK	4
 #define ERROR 5
 
-#define ERROR_CODE_0 0
-#define ERROR_CODE_1 1
-#define ERROR_CODE_2 2
-#define ERROR_CODE_3 3
-#define ERROR_CODE_4 4
-#define ERROR_CODE_5 5
-#define ERROR_CODE_6 6
-#define ERROR_CODE_7 7
+typedef enum {
+	UNKNOWN=0,
+	FILE_NOT_FOUND,
+	ACCESS_VIOLATION,
+	DISK_FULL,
+	ILLEGAL_OPERATION,
+	UNKNOWN_TRANSFER_ID,
+	FILE_EXISTS,
+	NO_SUCH_USER
+} ErrorCode;
 
-
-#define ERROR_0 "Unknown error.\0"
-#define ERROR_1 "File not found.\0"
-#define ERROR_2 "Access violation.\0"
-#define ERROR_3 "Disk full or allocation exceeded.\0"
-#define ERROR_4 "Illegal TFTP operation.\0"
-#define ERROR_5 "Unknown transfer ID.\0"
-#define ERROR_6 "File already exists.\0"
-#define ERROR_7 "No such user.\0"
+const char * const err_msg[] = {
+	"Unknown error.",
+	"File not found.",
+	"Access violation.",
+	"Disk full or allocation exceeded.",
+	"Illegal TFTP operation.",
+	"Unknown transfer ID.",
+	"File already exists.",
+	"No such user."
+	};
 
 struct sockaddr_in server, client;
 char reply[516];
-int error_code;
 unsigned int sockfd, buffer_size, iterator;
 int datablock;
 unsigned long block_code;
-FILE *data, *temp;
+FILE *data;
 
 //Write a data package to client.
-void write_data_package() {
+void send_data_packet() {
 	memset(reply, 0, sizeof(reply)); // Clean reply
 	buffer_size = 0;
 	//Add operation code 3
@@ -77,7 +79,8 @@ void write_data_package() {
 }
 
 //Send an error back to client.
-void write_error() {
+void send_error_response(ErrorCode error_code) {
+
 	memset(reply, 0, sizeof(reply));
 	//Add operation code 5
 	reply[0] = (5 >> 8) & 0xff;
@@ -86,43 +89,13 @@ void write_error() {
 	reply[2] = (error_code >> 8) & 0xff;
 	reply[3] = error_code & 0xff;
 	//Add appropriate string.
-	if (error_code == ERROR_CODE_0) {		//Unknown error.
-		buffer_size = sizeof(ERROR_0);
-		strncpy(&reply[4], ERROR_0, buffer_size);
-	}
-	else if (error_code == ERROR_CODE_1) {	//File not found.
-		buffer_size = sizeof(ERROR_1);
-		strncpy(&reply[4], ERROR_1, buffer_size);
-	}
-	else if (error_code == ERROR_CODE_2) {	//Access violation.
-		buffer_size = sizeof(ERROR_2);
-		strncpy(&reply[4], ERROR_2, buffer_size);
-	}
-	else if (error_code == ERROR_CODE_3) {	//Disk full or allocation exceeded.
-		buffer_size = sizeof(ERROR_3);
-		strncpy(&reply[4], ERROR_3, buffer_size);
-	}
-	else if (error_code == ERROR_CODE_4) {	//Illegal TFTP operation.
-		buffer_size = sizeof(ERROR_4);
-		strncpy(&reply[4], ERROR_4, buffer_size);
-	}
-	else if (error_code == ERROR_CODE_5) {	//Unknown transfer ID.
-		buffer_size = sizeof(ERROR_5);
-		strncpy(&reply[4], ERROR_5, buffer_size);
-	}
-	else if (error_code == ERROR_CODE_6) {	//File already exists.
-		buffer_size = sizeof(ERROR_6);
-		strncpy(&reply[4], ERROR_6, buffer_size);
-	}
-	else if (error_code == ERROR_CODE_7) {	//No such user.
-		buffer_size = sizeof(ERROR_7);
-		strncpy(&reply[4], ERROR_7, buffer_size);
-	}
-	printf("Sending ERROR message: \"");
-	for (iterator = 0; iterator < buffer_size; iterator++) {
-		printf("%c",reply[iterator+4]);
-	}
-	printf("\"\n");
+
+	strcpy(&reply[4], err_msg[error_code]);
+
+	printf("Sending ERROR message: \"%s\"\n", err_msg[error_code]);
+
+	// 4 bytes + string + '\0'
+	sendto(sockfd, reply, 4+strlen(err_msg[error_code])+1, 0, (struct sockaddr *) &client, (socklen_t) sizeof(client));
 
 	if (data != NULL) { //Close datastream, if file is open.
 		fclose(data);
@@ -131,7 +104,7 @@ void write_error() {
 }
 
 
-bool prefix(const char *prefix, const char *string) {
+bool is_prefix(const char *prefix, const char *string) {
     return strncmp(prefix, string, strlen(prefix)) == 0;
 }
 
@@ -173,7 +146,6 @@ int main(int argc, char *argv[]) {
 	char *ptr, *mode, *filename;
 	unsigned int op_code, mode_pointer;
 	unsigned long recieved_block_code;
-
 
 	// checking number of arguments
 	if (argc != 3) {
@@ -229,6 +201,9 @@ int main(int argc, char *argv[]) {
 
 		switch(op_code) {
 			case RRQ:
+				//Flush reply
+				memset(reply, 0, sizeof(reply));
+
 				//Extract filename from message.
 				filename = (char*)&message[2];
 				//find mode
@@ -240,20 +215,21 @@ int main(int argc, char *argv[]) {
 				memset(stream_path, 0, sizeof(stream_path));
 				strncpy(stream_path, shared_directory, strlen(shared_directory));
 				strncat(stream_path, "/", 1);
-				strncat(stream_path, filename, sizeof(stream_path)-strlen(shared_directory)+1);
+				// -2 because \0 is not included and "/" from previous line
+				strncat(stream_path, filename, sizeof(stream_path)-strlen(shared_directory)-2);
 
 				// checking if req. file is inside shared directory
 				printf("Filename is: %s\n", filename);
 				printf("Mode is %s\n", mode);
 
 				ptr = realpath(stream_path, absolute_file_path);
-				if (ptr == NULL)
-					perror("Error: ");
-				else if (!prefix(shared_directory_path, absolute_file_path)) {
-					error_code = ERROR_CODE_1;
-					write_error();
-					sendto(sockfd, reply, buffer_size+4, 0,
-					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
+				if (ptr == NULL) { //File not found. Write error message.
+					perror("Error");
+					send_error_response(FILE_NOT_FOUND);
+					break;
+				}
+				else if (!is_prefix(shared_directory_path, absolute_file_path)) {
+					send_error_response(FILE_NOT_FOUND);
 					printf("File is outside shared directory.\n[[ Access forbidden ]]\n");
 					break;
 				}
@@ -272,7 +248,7 @@ int main(int argc, char *argv[]) {
 					char temp_file_name[] = "/tmp/fileXXXXXX";
 					int fd = mkstemp(temp_file_name);
 					close(fd);
-					temp = fopen(temp_file_name, "w+"); // empty file
+					FILE *temp = fopen(temp_file_name, "w+"); // empty file
 
 					convert_to_nvt_ascii(data, temp);
 					fseek(temp, 0, SEEK_SET);
@@ -281,41 +257,21 @@ int main(int argc, char *argv[]) {
 					data = temp; // changing original file to temporary (with edited EOL)
 				}
 
-
-				//Flush reply
-				memset(reply, 0, sizeof(reply));
-				if (data == NULL) {	//File not found. Write error message.
-					error_code = ERROR_CODE_1;
-					write_error();
-				}
-				else {				//File found. Begin sending data.
-					block_code = 1;
-					write_data_package();
-				}
+				// Begin sending data.
+				block_code = 1;
+				send_data_packet();
 				break;
+
 			case WRQ:
-				//Function to test case 3.
-				/*
-				reply[0] = (4 >> 8) & 0xff;
-				reply[1] = 4 & 0xff;
-				reply[2] = (0 >> 8) &0xff;
-				reply[3] = 0 &0xff;
-				sendto(sockfd, reply, 4, 0,
-					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
+				// Write Requests forbidden. Write error message.
+				send_error_response(ACCESS_VIOLATION);
 				break;
-				*/
 
-				error_code = ERROR_CODE_2; //Write Requests forbidden. Write error message.
-				write_error();
-				sendto(sockfd, reply, buffer_size+4, 0,
-					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
-				break;
 			case DATA:
-				error_code = ERROR_CODE_4; //Illegal FTTP operation. Write error message.
-				write_error();
-				sendto(sockfd, reply, buffer_size+4, 0,
-					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
+				// Illegal FTTP operation. Write error message.
+				send_error_response(ILLEGAL_OPERATION);
 				break;
+
 			case ACK:
 				//Fetch block code from message
 				recieved_block_code = (((unsigned char*)message)[2] << 8) + ((unsigned char*)message)[3];
@@ -330,7 +286,7 @@ int main(int argc, char *argv[]) {
 					else { //Send next package.
 						block_code++;
 						fprintf(stdout, "Blockode to be sent is: %lu\n", block_code);
-						write_data_package();
+						send_data_packet();
 					}
 				}
 				else { //Block code mismatch. Resend last package.
@@ -339,6 +295,7 @@ int main(int argc, char *argv[]) {
 						  (struct sockaddr *) &client, (socklen_t) sizeof(client));
 				}
 				break;
+
 			case ERROR:
 				fprintf(stdout, "Error code %d: %s\n", message[3], &message[4]);
 				if (data != NULL) { //Close datastream, if file is open.
