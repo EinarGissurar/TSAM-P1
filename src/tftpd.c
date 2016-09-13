50fed03b5550b1de7dd183a9c5440457d63cd8aa
Peter Hostačný <hostacny.peter@gmail.com>
2016-09-13 03:31:36 +0200

    Checking path, handling signals, format output

--- src/tftpd.c
+++ src/tftpd.c
@@ -8,6 +8,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <limits.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <signal.h>
 
 #define RRQ 1
 #define WRQ 2
@@ -31,23 +35,23 @@
 #define ERROR_3 "Disk full or allocation exceeded.\0"
 #define ERROR_4 "Illegal TFTP operation.\0"
 #define ERROR_5 "Unknown transfer ID.\0"
-#define ERROR_6 "File already esists.\0"
+#define ERROR_6 "File already exists.\0"
 #define ERROR_7 "No such user.\0"
 
 struct sockaddr_in server, client;
 
-char message[516], reply[516], stream_path[100];
+char message[516], reply[516], stream_path[255];
 char* filename;
 char* mode;
 int error_code;
-unsigned int sockfd, port_number, op_code, mode_pointer, buffer_size, i;
+unsigned int sockfd, op_code, mode_pointer, buffer_size, i;
 int datablock;
 unsigned long block_code, block_reply;
 FILE *data;
 
 void write_reply() {
-	memset(reply, 0, strlen(reply)); // Clean reply
-				
+	memset(reply, 0, sizeof(reply)); // Clean reply
+
 	buffer_size = 0;
 	reply[0] = (3 >> 8) & 0xff;
 	reply[1] = 3 & 0xff;
@@ -66,7 +70,7 @@ void write_reply() {
 }
 
 void write_error() {
-	memset(reply, 0, strlen(reply));
+	memset(reply, 0, sizeof(reply));
 	reply[0] = (5 >> 8) & 0xff;
 	reply[1] = 5 & 0xff;
 	reply[2] = (error_code >> 8) & 0xff;
@@ -103,76 +107,124 @@ void write_error() {
 		buffer_size = sizeof(ERROR_7);
 		strncpy(&reply[4], ERROR_7, buffer_size);
 	}
-	puts(reply);
+	//puts(reply);
+	printf("Sending ERROR message: \"");
 	for (i = 0; i < buffer_size; i++) {
-		fprintf(stdout, "%c",reply[i+4]);
+		printf("%c",reply[i+4]);
 	}
-	fprintf(stdout, "\n");
+	printf("\"\n");
 }
 
+
+bool prefix(const char *prefix, const char *string) {
+    return strncmp(prefix, string, strlen(prefix)) == 0;
+}
+
+void sig_handler(int signal_n) {
+	if (signal_n == SIGINT)
+		printf("\nShutting down...\n");
+
+	/* Close the connection. */
+	shutdown(sockfd, SHUT_RDWR);
+	close(sockfd);
+
+	exit(0);
+}
+
+
 int main(int argc, char *argv[]) {
-	port_number = strtol(argv[1], NULL, 10);
+
+	char shared_directory_path [PATH_MAX+1], absolute_file_path [PATH_MAX+1];
+	char *ptr;
+
+	// checking number of arguments
+	if (argc != 3) {
+		printf("Usage: %s <port> <folder_name>\n", argv[0]);
+		return 1;
+	}
+
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+		printf("\nCannot catch SIGINT!\n");
+
+	int port_number = strtol(argv[1], NULL, 10);
+	char *shared_directory = argv[2];
 
 	/* Create and bind a TCP socket */
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	/* Network functions need arguments in network byte order instead of 
+	/* Network functions need arguments in network byte order instead of
 	host byte order. The macros htonl, htons convert the values. */
 	memset(&server, 0, sizeof(server));
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
 	server.sin_port = htons(port_number);
-	bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+	if (bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server)) < 0) {
+		perror("Bind failed. Error");
+		return 1;
+	}
+	listen(sockfd, 1);
 
 	fprintf(stdout, "The server is on\n");
-	//fprintf(stdout, "argv[0] is: %s \n", argv[0]);
-	//fprintf(stdout, "argv[1] is: %s \n", argv[1]);
-	//fprintf(stdout, "argv[2] is: %s \n", argv[2]);
-	//fprintf(stdout, "argv[3] is: %s \n", argv[3]);
-	//fprintf(stdout, "Port number is: %i \n", port_number);
-	
-	fflush(stdout);
-	listen(sockfd, 1);
-	
+	ptr = realpath(shared_directory, shared_directory_path);
+	if (ptr == NULL) {
+		perror("Error: ");
+		return(42);
+	}
+	// for making sure that prefix doesn't match file with similar name
+	strcat(shared_directory_path, "/");
+	printf("shared_directory_path: %s\n", shared_directory_path);
+
 	while(1) {
 
-		fprintf(stdout, "Waiting...\n");
+		printf("\nWaiting...\n");
 
 		/* We first have to accept a TCP connection, connfd is a fresh
 		   handle dedicated to this connection. */
 		socklen_t len = (socklen_t) sizeof(client);
-	  
+
 		/* Receive from connfd, not sockfd. */
-		ssize_t n = recvfrom(sockfd, message, sizeof(message) - 1, 0, 
+		ssize_t n = recvfrom(sockfd, message, sizeof(message) - 1, 0,
 			(struct sockaddr *) &client, &len);
-		fprintf(stdout, "Size of n: %zu\n", n);
+		printf("Size of received message [bytes]: %zu\n", n);
 		op_code = message[1];
-		fprintf(stdout, "Opcode is: %d\n", op_code);
+		printf("Opcode is: %d\n", op_code);
 
 		switch(op_code) {
 			case RRQ:
 				//message to read/write
 				filename = (char*)&message[2];
-
-				//path to stream
-				memset(stream_path, 0, strlen(stream_path));
-				strcpy(stream_path, argv[2]);
-				strcat(stream_path, "/");
-				strcat(stream_path, filename);
-
 				//find mode
 				for (mode_pointer = 2; message[mode_pointer] != '\0'; mode_pointer++) {}
 				mode_pointer++;
 				mode = &message[mode_pointer];
 
-				fprintf(stdout, "Filename is: %s\n", filename);
-				fprintf(stdout, "Mode is %s\n", mode);
-				fprintf(stdout, "Path is %s\n", stream_path);
-				fflush(stdout);
+				//path to stream
+				memset(stream_path, 0, sizeof(stream_path));
+				strcpy(stream_path, shared_directory);
+				strcat(stream_path, "/");
+				strcat(stream_path, filename);
+				// checking if req. file is inside shared directory
+
+				printf("Filename is: %s\n", filename);
+				printf("Mode is %s\n", mode);
+
+				ptr = realpath(stream_path, absolute_file_path);
+				if (ptr == NULL)
+					perror("Error: ");
+				else if (!prefix(shared_directory_path, absolute_file_path)) {
+					error_code = ERROR_CODE_1;
+					write_error();
+					sendto(sockfd, reply, buffer_size+4, 0,
+					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
+					printf("File is outside shared directory.\n[[ Access forbidden ]]\n");
+					break;
+				}
+				else
+					printf("Path is %s\n", absolute_file_path);
 
 				//Flush reply
 				data = fopen(stream_path, "r");
-				memset(reply, 0, strlen(reply));
+				memset(reply, 0, sizeof(reply));
 				if (data == NULL) {
 					error_code = ERROR_CODE_1;
 					write_error();
@@ -182,31 +234,31 @@ int main(int argc, char *argv[]) {
 					block_code = 1;
 					write_reply();
 				}
-				sendto(sockfd, reply, buffer_size+4, 0, 
+				sendto(sockfd, reply, buffer_size+4, 0,
 					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
-			break;
+				break;
 			case WRQ:
 				//Function to test case 3.
-				/*	
+				/*
 				reply[0] = (4 >> 8) & 0xff;
 				reply[1] = 4 & 0xff;
 				reply[2] = (0 >> 8) &0xff;
 				reply[3] = 0 &0xff;
-				sendto(sockfd, reply, 4, 0, 
+				sendto(sockfd, reply, 4, 0,
 					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
 			break; */
 
 				error_code = ERROR_CODE_2;
 				write_error();
-				sendto(sockfd, reply, buffer_size+4, 0, 
+				sendto(sockfd, reply, buffer_size+4, 0,
 					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
-			break;
+				break;
 			case DATA:
 				error_code = ERROR_CODE_4;
 				write_error();
-				sendto(sockfd, reply, buffer_size+4, 0, 
+				sendto(sockfd, reply, buffer_size+4, 0,
 					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
-			break;
+				break;
 			case ACK:
 				block_reply = (((unsigned char*)message)[2] << 8) + ((unsigned char*)message)[3];
 				fprintf(stdout, "Blockode reply = %lu\n", block_reply);
@@ -214,7 +266,7 @@ int main(int argc, char *argv[]) {
 				if (block_code == block_reply) {
 					fprintf(stdout, "Data recieved\n");
 					fflush(stdout);
-					
+
 					block_code++;
 					fprintf(stdout, "Blockode to be sent is: %lu\n", block_code);
 					write_reply();
@@ -223,17 +275,13 @@ int main(int argc, char *argv[]) {
 					sleep(1);
 					fprintf(stdout, "Resend last package\n");
 				}
-				sendto(sockfd, reply, buffer_size+4, 0, 
+				sendto(sockfd, reply, buffer_size+4, 0,
 					  (struct sockaddr *) &client, (socklen_t) sizeof(client));
-			break;
+				break;
 			case ERROR:
 				fprintf(stdout, "Error code %d: %s\n", message[3], &message[4]);
-			break;
+				break;
 		}
-		
-		/* Close the connection. */
-		//shutdown(connfd, SHUT_RDWR);
-		//close(connfd);
 	}
 	return 0;
 }
